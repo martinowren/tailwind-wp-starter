23fa8a723b3831b9c599e2802f2030e35c7259d8
Martin Owren <martin.owren@gmail.com>
2021-02-06 19:50:28 +0100

    📦 Helper functions

---
+++ src/helpers/assets.php
@@ -0,0 +1,9 @@
+<?php
+
+function theme_asset_complete_uri($path) {
+	return get_stylesheet_directory_uri() . '/' . $path;
+}
+
+function theme_asset_complete_path($path) {
+	return get_template_directory() . '/' . $path;
+}

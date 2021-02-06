7398f8ef0863b1fbe2becbf67b7f0ed71bc99d8d
Martin Owren <martin.owren@gmail.com>
2021-02-06 19:51:27 +0100

    🔫 Lint

--- index.php
+++ index.php
@@ -14,11 +14,10 @@
 
 get_header();
 
-	if( have_posts() ) {
-
-		while( have_posts() ) {
-			the_post();
-		}
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
 	}
+}
 
 get_footer();
--- single.php
+++ single.php
@@ -1,6 +1,5 @@
 
-<?php
-/**
+<?php /**
  * The template for displaying all single posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
@@ -8,24 +7,29 @@
  * @package WPMODEVSTARTER
  */
 
-get_header();
-?>
+get_header(); ?>
 		<div class="single-blog-parent">
 			<article class="single-blog">
 				<div class="single-blog-title">
-					<?php the_post_thumbnail( 'large' );?>
+					<?php the_post_thumbnail('large'); ?>
 					<div class="text-section">
 						<div class="text">	
 							<h1><?php the_title(); ?></h1>
-							<p><?php echo get_the_date('d') . '.' . get_the_date('m') . '.' . get_the_date('y'); ?></p>
+							<p><?php echo get_the_date('d') .
+       	'.' .
+       	get_the_date('m') .
+       	'.' .
+       	get_the_date('y'); ?></p>
 						</div>
 					</div>
 				</div>
 				<div class="single-blog-content">
-						<?php if ( have_posts() ) : ?>
-							<?php while ( have_posts() ) : the_post(); ?>
-								<?php the_content();?>
-							<?php endwhile; ?>
+						<?php if (have_posts()): ?>
+							<?php while (have_posts()):
+       	the_post(); ?>
+								<?php the_content(); ?>
+							<?php
+       endwhile; ?>
 						<?php endif; ?>
 				</div>
 			</article>
@@ -33,5 +37,4 @@ get_header();
 
 
 
-<?php
-get_footer();
+<?php get_footer();

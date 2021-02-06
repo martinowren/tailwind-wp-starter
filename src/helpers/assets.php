6ef42ea8f1d4880b0035292092016ae28cb8ae6f
martinowren <martin.owren@gmail.com>
2021-02-06 21:41:20 +0100

    🔫🔫Linted

--- src/helpers/assets.php
+++ src/helpers/assets.php
@@ -1,9 +1,11 @@
 <?php
 
-function theme_asset_complete_uri($path) {
+function theme_asset_complete_uri($path)
+{
 	return get_stylesheet_directory_uri() . '/' . $path;
 }
 
-function theme_asset_complete_path($path) {
+function theme_asset_complete_path($path)
+{
 	return get_template_directory() . '/' . $path;
 }
--- src/helpers/filters.php
+++ src/helpers/filters.php
@@ -3,17 +3,17 @@
  * Add <body> classes
  */
 add_filter('body_class', function (array $classes) {
-    /** Add page slug if it doesn't exist */
-    if (is_single() || is_page() && !is_front_page()) {
-        if (!in_array(basename(get_permalink()), $classes)) {
-            $classes[] = basename(get_permalink());
-        }
-    }
+	/** Add page slug if it doesn't exist */
+	if (is_single() || (is_page() && !is_front_page())) {
+		if (!in_array(basename(get_permalink()), $classes)) {
+			$classes[] = basename(get_permalink());
+		}
+	}
 
-    /** Add class if sidebar is active */
-    if (display_sidebar()) {
-        $classes[] = 'sidebar-primary';
-    }
+	/** Add class if sidebar is active */
+	if (display_sidebar()) {
+		$classes[] = 'sidebar-primary';
+	}
 
-    return array_filter($classes);
+	return array_filter($classes);
 });

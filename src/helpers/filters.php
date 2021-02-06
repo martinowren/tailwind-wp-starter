5f14fb63ec202c70f3e36f2eb50865e94d469a59
Martin Owren <martin.owren@gmail.com>
2021-02-06 19:50:40 +0100

    📦 Body filters

---
+++ src/helpers/filters.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Add <body> classes
+ */
+add_filter('body_class', function (array $classes) {
+    /** Add page slug if it doesn't exist */
+    if (is_single() || is_page() && !is_front_page()) {
+        if (!in_array(basename(get_permalink()), $classes)) {
+            $classes[] = basename(get_permalink());
+        }
+    }
+
+    /** Add class if sidebar is active */
+    if (display_sidebar()) {
+        $classes[] = 'sidebar-primary';
+    }
+
+    return array_filter($classes);
+});
